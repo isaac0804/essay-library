d1943511016b4e4c71e19306c93b5ee952a0d7fe
isaac <isaacyong0804@gmail.com>
2024-12-24 21:08:16 +0000

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js'],
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
